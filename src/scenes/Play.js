9799a7725498c9bb43db4f6067f3d3ad1701de13
Allison Pham <alapham@ucsc.edu>
2021-04-09 21:04:37 -0700

    Point System

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -80,6 +80,21 @@ class Play extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}),
             frameRate: 30
         });
+
+        //score default
+        this.p1Score = 0;
+
+        //score display
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {top: 5, bottom: 5},
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
     }
 
     update() {
@@ -135,5 +150,7 @@ class Play extends Phaser.Scene {
             ship.alpha = 1;
             boom.destroy();
         });
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }
